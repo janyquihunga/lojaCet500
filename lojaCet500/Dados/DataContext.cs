9310d7bb04af90c742fdc24e98b275c73778d0bc
Janizi Quihunga <Janizi.Neves.Quihunga@formandos.cinel.pt>
2021-01-05 21:34:57 +0000

    Criar base de dados e tabela produtos.

---
+++ lojaCet500/Dados/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using lojaCet500.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace lojaCet500.Dados
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Produto> Produtos { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ lojaCet500/Dados/Entidades/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace lojaCet500.Dados.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "image")]
+        public string UrlDaIamgem { get; set; }
+
+        [Display(Name = "Última compra")]
+        public DateTime UltimaCompra { get; set; }
+
+        [Display(Name = "Última venda")]
+        public DateTime UltimaVenda { get; set; }
+
+        [Display(Name = "Disponível")]
+        public bool Disponivel { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+
+    }
+}
